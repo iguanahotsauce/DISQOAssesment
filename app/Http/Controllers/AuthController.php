1ff1054ae44fa3c76aa9886f8b1fe61aed42f131
Alex Beebe <ubuntu@ip-172-31-37-160.us-west-2.compute.internal>
2021-05-19 23:12:16 +0000

    Add a register function to the AuthController to create a new user account

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    // Function to create a new user
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'email' => 'email|required|unique:users,email',
+            'password' => 'string|required'
+        ]);
+
+        $user = User::create([
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('DISQOAssesmentToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
 }
