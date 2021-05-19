e1134544c9816a4a6f8f6d864331be963504f076
Alex Beebe <ubuntu@ip-172-31-37-160.us-west-2.compute.internal>
2021-05-19 23:45:51 +0000

    Create the Note Controller, Note Model, and Notes Table

---
+++ database/migrations/2021_05_19_232956_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->string('email');
+            $table->string('title')->max(50);
+            $table->string('note')->max(1000)->nullable();
+            $table->timestamps();
+
+            $table->foreign('email')->references('email')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
