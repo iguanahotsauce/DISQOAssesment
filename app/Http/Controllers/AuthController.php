462d96818647dc21aa9c75c7762e235bdd281348
Alex Beebe <ubuntu@ip-172-31-37-160.us-west-2.compute.internal>
2021-05-19 23:27:37 +0000

    Create a login function for a user that has already created an account to login and receive a new token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,6 +31,36 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    // Function to login a user that has already created an account
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'email|required',
+            'password' => 'string|required'
+        ]);
+
+        // Check given email to make sure it exists in the users table
+        $user = User::where('email', $fields['email'])->first();
+
+        // If the email does exist in the users table then check the given password to make sure that it matches, if it doesn't or if the
+        // user does not exist in the users table return a 401
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            $response = [
+                'message' => 'Invalid Credentials'
+            ];
+
+            return response($response, 401);
+        }
+
+        $token = $user->createToken('DISQOAssesmentToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
     // Delete all tokens in the DB associated with the user that is logging out
     public function logout(Request $request) {
         auth()->user()->tokens()->delete();
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/logout', [AuthController::class, 'logout']);
