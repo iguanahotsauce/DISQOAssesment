720a842fc6d98dc3a56c333d7ed055f04ca85a63
Alex Beebe <alexbeebe@icloud.com>
2021-05-20 14:22:12 -0700

    Everything can be done with the default functionality of the Note model, we don't need the extra functions

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -47,14 +47,16 @@ class NoteController extends Controller
 
     /**
      * Returns all notes associated with the logged in user
+     * 
+     * @return  JSON response with with the specific note
      */
     public function index() {
+        // Get the users email from the token using Sanctum
         $user = auth('sanctum')->user();
 
-        $note = new Note;
-
+        // Get all of the notes associated with the user from the Note model and return them with a 200 response
         $response = [
-            'notes' => $note->getNote($user->email)
+            'notes' => Note::where('email', $user->email)->get()
         ];
 
         return response($response, 200);
@@ -64,30 +66,38 @@ class NoteController extends Controller
      * Returns a specific note
      * 
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return  JSON response with with the specific note
      */
     public function show($id)
     {
+        // Get the users email from the token using Sanctum
         $user = auth('sanctum')->user();
 
-        $note = new Note;
+        // Get the note for the given ID from the DB but also make sure to check that it was created by the current user
+        $note = Note::where('email', $user->email)->where('id', $id)->get();
+
+        // If there is no note returned from the DB return a 400 response with an error message saying no note exists
+        if(!count($note)) {
+            return response(['errors' => 'No note exists for the given ID'], 400);
+        }
 
         $response = [
-            'notes' => $note->getNote($user->email, $id)
+            'notes' => $note
         ];
 
         return response($response, 200);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update the note with the given id for the user associated with the token sent
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return JSON response with success/failure message
      */
     public function update(Request $request, $id)
     {
+        // First validate the incoiming data from the request to make sure it fits our parameters
         $validator = Validator::make($request->all(), [
             'title' => 'required|max:50',
             'note' => 'max:1000'
@@ -99,38 +109,43 @@ class NoteController extends Controller
             return response(['errors' => $validator->messages()], 400);
         }
 
-        // Passed validation, select the note from the database
-
-        $note = new Note;
-
         $request_data = [
             'title' => $request->title,
             'note' => $request->note
         ];
 
-        $response = [
-            'message' => $note->updateNote($request_data, $id, $request->user()->email)
-        ];
-        
-        return response($response, 200);
+        $update = Note::where('email', $request->user()->email)->where('id', $id)->update($request_data);
+
+        // If there is no note updated return a 400 response with an error message saying no note exists
+        if($update != 1) {
+            return response(['errors' => 'No note exists for the given ID'], 400);
+        }
+
+        // The note exists and was created by the current user, return a 200 respoonse with a success message
+
+        return response(['message' => 'Note was successfully updated'], 200);
     }
 
     /**
-     * Delete the note with the given title for the user associated with the token sent
+     * Delete the note with the given id for the user associated with the token sent
      *
      * @param  int  $id 
-     * @return \Illuminate\Http\Response
+     * @return JSON response with success/failure message
      */
     public function destroy($id)
     {
+        // Get the users email from the token using Sanctum
         $user = auth('sanctum')->user();
 
-        $note = new Note;
+        $delete = Note::where('email', $user->email)->where('id', $id)->delete();
 
-        $response = [
-            'message' => $note->removeNote($id, $user->email)
-        ];
+        // If there is no note deleted return a 400 response with an error message saying no note exists
+        if($delete != 1) {
+            return response(['errors' => 'No note exists for the given ID'], 400);
+        }
 
-        return response($response, 200);
+        // The note exists and was created by the current user, return a 200 respoonse with a success message
+
+        return response(['message' => 'Note was successfully deleted'], 200);
     }
 }
